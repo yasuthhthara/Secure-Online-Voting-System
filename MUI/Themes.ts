f3e7fa4da6d89c406ccf70fff4c43dac9c69d4f7
Vihangi Yasuththara <vihangi1999gamage@gmail.com>
2023-05-11 01:15:31 +0530

    create a theme

---
+++ MUI/Themes.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FFFFF',
+    },
+    secondary: {
+      main: '#686868',
+    },
+  },
+});
+
+export default theme;
